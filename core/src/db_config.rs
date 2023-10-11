0813bd6de63cde0ca1ddfeb706c9f45fb0d37f1c
Illia Stetsenko <illia.stetsenko@netstalker.io>
2023-10-11 12:22:40 +0300

    feature/db-macros-config: removed db-config module

--- core/src/db_config.rs
+++
@@ -1,8 +0,0 @@
-use std::collections::HashMap;
-use std::rc::Rc;
-use crate::types::CellValue;
-
-pub trait DbConfig {
-    #[allow(clippy::type_complexity)]
-    fn get_supported_types() -> HashMap<String, fn(String) -> Rc<dyn CellValue>>;
-}
